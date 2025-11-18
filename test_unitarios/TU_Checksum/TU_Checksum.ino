6b08b1c00d0cc307bcedbef9b6ce6bb9310d02dc
isilcobo728-spec <isil.cobo.728@gmail.com>
2025-11-18 09:57:49 +0100

    Add files via upload

---
+++ test_unitarios/TU_Checksum/TU_Checksum.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+
+
+int Checksum(char str[]) {
+    int caracter, suma = 0;
+    for(int i = 0; str[i] != '\0'; i++) {
+        caracter = str[i];
+        suma = suma + caracter;
+    }
+    int checksum = (suma)%256;
+    return(checksum);
+}
+
+char frase[100] = "Hola, mundo!";
+char frase2[100] = "Hola mundo!";
+char frase3[100] = "Hola. mundo!";
+
+void setup() {
+  Serial.begin(9600);  
+}
+
+void loop() {
+  Serial.println(Checksum(frase));
+  Serial.println(Checksum(frase2));
+  Serial.println(Checksum(frase3));
+  delay(5000);
+
+}
