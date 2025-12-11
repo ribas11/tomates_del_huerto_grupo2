d846ad24885e2660cdb74ac8e35a1212a64f068f
rogerjunca-png <roger.junca@estudiantat.upc.edu>
2025-12-11 14:59:05 +0100

    Test unitario ckecksum (potser noo va)

--- test_unitarios/TU_Checksum/TU_Checksum.ino
+++ test_unitarios/TU_Checksum/TU_Checksum.ino
@@ -1,28 +1,40 @@
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 
 
-int Checksum(char str[]) {
-    int caracter, suma = 0;
-    for(int i = 0; str[i] != '\0'; i++) {
-        caracter = str[i];
-        suma = suma + caracter;
-    }
-    int checksum = (suma)%256;
-    return(checksum);
+// int Checksum(char str[]) {
+//     int caracter, suma = 0;
+//     for(int i = 0; str[i] != '\0'; i++) {
+//         caracter = str[i];
+//         suma = suma + caracter;
+//     }
+//     int checksum = (suma)%256;
+//     return(checksum);
+// }
+
+// char frase[100] = "Hola, mundo!";
+// char frase2[100] = "Hola mundo!";
+// char frase3[100] = "Hola. mundo!";
+
+// void setup() {
+//   Serial.begin(9600);  
+// }
+
+// void loop() {
+//   Serial.println(Checksum(frase));
+//   Serial.println(Checksum(frase2));
+//   Serial.println(Checksum(frase3));
+//   delay(5000);
+
+// }
+
+int Checksum(const char str[]) {
+  unsigned int suma = 0;          // acumulador
+
+  for (int i = 0; str[i] != '\0'; i++) {
+    suma += (unsigned char)str[i]; // sumar valor ASCII de cada carácter
+  }
+
+  return suma % 256;              // checksum de 8 bits (0..255)
 }
 
-char frase[100] = "Hola, mundo!";
-char frase2[100] = "Hola mundo!";
-char frase3[100] = "Hola. mundo!";
 
-void setup() {
-  Serial.begin(9600);  
-}
-
-void loop() {
-  Serial.println(Checksum(frase));
-  Serial.println(Checksum(frase2));
-  Serial.println(Checksum(frase3));
-  delay(5000);
-
-}
